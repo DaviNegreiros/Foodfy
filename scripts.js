7d2745b4e0f10690888ab616e4b0e7e8c5138889
Davi Negreiros <75706873+DaviNegreiros@users.noreply.github.com>
2021-02-25 09:57:34 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards) {
+    card.addEventListener("click", function(){
+        const imgId = card.getAttribute("id")
+        modalOverlay.classList.add('active')
+        modalOverlay.querySelector("#recipe-img").src = `/Foodfy/assets/${imgId}.png`;
+    })
+}
+
+document.querySelector(".close-modal").addEventListener("click", function() {
+    modalOverlay.classList.remove("active")
+})
+
+
+
+
